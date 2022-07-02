75fd5b831b6a85d101b46c67147d4b6e1a185dab
ASM717 <stepan151@gmail.com>
2022-07-02 11:20:26 +0300

    palindorme

--- src/easy/PalindromeNumber.java
+++ src/easy/PalindromeNumber.java
@@ -12,7 +12,7 @@ public class PalindromeNumber {
         temp = x;
         while (x > 0) {
             r = x % 10;
-            sum = (sum*10) + r;
+            sum = (sum * 10) + r;
             x = x/10;
         }
         return temp == sum;
