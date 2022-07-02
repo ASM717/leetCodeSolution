327122e4c16d131566a2978b4b3454091a861fa0
ASM717 <stepan151@gmail.com>
2022-07-02 15:04:41 +0300

    MergeTwoSortedLists

---
+++ src/easy/MergeTwoSortedLists.java
@@ -0,0 +1,33 @@
+package easy;
+
+public class MergeTwoSortedLists {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode res = null;
+
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        if ((list1.val) <= (list2.val)) {
+            res = list1;
+            res.next = mergeTwoLists(list1.next, list2);
+        }
+        else {
+            res = list2;
+            res.next = mergeTwoLists(list1, list2.next);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
